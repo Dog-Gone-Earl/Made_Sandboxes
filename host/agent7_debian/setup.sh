84019c7bf87f743045afc79a537c5dfad890ab60
Dog-Gone-Earl <terry.dennison@datadoghq.com>
2023-11-12 06:59:09 -0700

    Cleanup of repos

--- host/agent7_debian/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "alias dd_start='sudo systemctl start datadog-agent'">>~/.bashrc
-echo "alias dd_stop='sudo systemctl stop datadog-agent'">>~/.bashrc
-echo "alias dd_restart='sudo systemctl start datadog-agent && sudo systemctl stop datadog-agent'">>~/.bashrc
-echo "alias dd_status='sudo datadog-agent status'">>~/.bashrc
-source ~/.bashrc
-
